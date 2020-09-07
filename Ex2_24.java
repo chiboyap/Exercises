fa0146eb0f4bcefc12fe08e7c1e557da9c44eebf
chiboyap <60558376+chiboyap@users.noreply.github.com>
2020-09-07 03:09:55 +0100

    Add files via upload

---
+++ Ex2_24.java
@@ -0,0 +1,28 @@
+/* Created By Alionye Chinaemerem Paschal
+ * on 8/31/2020 - by 3:35 PM
+ */
+
+import java.util.Scanner;
+
+public class Ex2_24 {
+    public static void main (String [] args){
+        int large; //declare variable large
+        int small; //declare variable small
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter first integer: ");
+        int a = input.nextInt();
+        System.out.println("Enter Second integer: ");
+        int b = input.nextInt();
+        System.out.println("Enter third integer: ");
+        int c = input.nextInt();
+        System.out.println("Enter fourth integer: ");
+        int d = input.nextInt();
+        System.out.println("Enter fifth integer: ");
+        int e = input.nextInt();
+
+        large = Math.max(a,b);
+        if (c )
+
+    }
+}
